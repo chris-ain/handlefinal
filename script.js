137c01e99c8f70f4c9141d2fcf0cccefc075ff8b
chris-ain <58142420+chris-ain@users.noreply.github.com>
2021-08-31 04:08:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -171,14 +171,7 @@ nameTL
 //const svg = document.querySelector("#overlay");
 const ratio = 0.5625;
 
-function newSize() {
-  let w = window.innerWidth;
-  let h = window.innerHeight;
-  if (w > h * (16 / 9)) {
-    gsap.set("#pin-overlay", { attr: { width: w, height: w * ratio } });
-  } else {
-    gsap.set("#pin-overlay", { attr: { width: h / ratio, height: h } });
-  }
+
   //let data = svg.getBoundingClientRect();
   //gsap.set("#overlay", {x:w/2 - data.width/2});
   //gsap.set("#overlay", {y:h/2 - data.height/2});
